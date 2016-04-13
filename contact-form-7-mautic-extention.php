a55edf8642ba6ba37e26a2efa8f6c12000ea852e
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-13 11:43:28 +0900

    check cf7 status

--- contact-form-7-mautic-extention.php
+++ contact-form-7-mautic-extention.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Contact-form-7-mautic-extention
- * Version: 0.1-alpha
+ * Plugin Name: Contact form 7 Mautic Extention
+ * Version: 0.0.1
  * Description: PLUGIN DESCRIPTION HERE
  * Author: YOUR NAME HERE
  * Author URI: YOUR SITE HERE
@@ -10,3 +10,34 @@
  * Domain Path: /languages
  * @package Contact-form-7-mautic-extention
  */
+
+if ( ! mautic_is_activate_cf7() {
+	return;
+}
+
+add_filter( 'wpcf7_before_send_mail', 'send_cf7_to_mautic' );
+function send_cf7_to_mautic( $cf7 ) {
+	if ( $submission = WPCF7_Submission::get_instance() ) {
+		$posted_data = $submission->get_posted_data();
+		var_dump($posted_data);
+	}
+	var_dump(true);
+	exit;
+	return $cf7;
+}
+
+/**
+ * Check Contact form 7 Plugin status
+ *
+ * @since 0.0.1
+ * @return bool
+ */
+function mautic_is_activate_cf7() {
+	$activePlugins = get_option('active_plugins');
+	$plugin = 'contact-form-7/wp-contact-form-7.php';
+	if ( ! array_search( $plugin, $activePlugins ) && file_exists( WP_PLUGIN_DIR. '/'. $plugin ) ) {
+		return false;
+	} else {
+		return true;
+	}
+}
