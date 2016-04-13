6ae3c3854150ee273015722bf47ca85dc976405c
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-13 10:26:15 +0900

    init

---
+++ contact-form-7-mautic-extention.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Contact-form-7-mautic-extention
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: contact-form-7-mautic-extention
+ * Domain Path: /languages
+ * @package Contact-form-7-mautic-extention
+ */
