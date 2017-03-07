5d5bf517aadac86cf94605ff152784ae075f6ec4
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-03-08 03:00:56 +0900

    use in_array

--- inc/class.cf7-checker.php
+++ inc/class.cf7-checker.php
@@ -1,11 +1,23 @@
 <?php
 
+/**
+ * Class CF7_Mautic_CF7_Checker
+ *
+ * Check installed Contact Form 7.
+ *
+ * @since 0.1.0
+ */
 class CF7_Mautic_CF7_Checker extends CF7_Mautic_Environment_Checker {
 
+	/**
+	 * Check installed cf7.
+	 *
+	 * @return bool|WP_Error
+	 */
 	public function check() {
 		$active_plugins = get_option( 'active_plugins' );
 		$plugin = 'contact-form-7/wp-contact-form-7.php';
-		if ( false !== array_search( $plugin, $active_plugins ) and file_exists( WP_PLUGIN_DIR . '/' . $plugin ) ) {
+		if ( in_array( $plugin, $active_plugins, true ) and file_exists( WP_PLUGIN_DIR . '/' . $plugin ) ) {
 			return true;
 		}
 
