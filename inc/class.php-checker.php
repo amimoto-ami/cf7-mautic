084cd76d0ad013bab753bd339df46faebca979ec
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-03-08 03:00:42 +0900

    add doc comments.

--- inc/class.php-checker.php
+++ inc/class.php-checker.php
@@ -1,33 +1,52 @@
 <?php
 
+/**
+ * Class CF7_Mautic_PHP_Checker
+ *
+ * @since 0.1.0
+ */
 class CF7_Mautic_PHP_Checker extends CF7_Mautic_Environment_Checker {
 
 	/**
-	 * @var string.
+	 * Support PHP ver.
+	 *
+	 * @var string
 	 */
-	private $support_php_version = CF7_MAUTIC_REQUIRE_PHP_VERSION;
+	private $support_php_version = '5.6';
 
 	/**
+	 * Current PHP ver.
+	 *
 	 * @var string
 	 */
 	private $current_php_version = '';
 
 	/**
-	 * @param string $current_php_version
+	 * CF7_Mautic_PHP_Checker constructor.
+	 */
+	public function __construct() {
+
+		if ( defined( 'CF7_MAUTIC_REQUIRE_PHP_VERSION' ) ) {
+			$this->set_support_php_version( CF7_MAUTIC_REQUIRE_PHP_VERSION );
+		}
+
+		$this->set_current_php_version( phpversion() );
+	}
+
+	/**
+	 * Setter for current_php_version.
+	 *
+	 * @param string $current_php_version php version.
 	 */
 	public function set_current_php_version( $current_php_version ) {
 		$this->current_php_version = $current_php_version;
 	}
 
-	/**
-	 * CF7_Mautic_PHP_Checker constructor.
-	 */
-	public function __construct() {
-		$this->set_current_php_version( phpversion() );
-	}
 
 	/**
-	 * @param string $support_php_version
+	 * Setter for support_php_version.
+	 *
+	 * @param string $support_php_version php version.
 	 */
 	public function set_support_php_version( $support_php_version ) {
 		$this->support_php_version = $support_php_version;
@@ -39,7 +58,7 @@ class CF7_Mautic_PHP_Checker extends CF7_Mautic_Environment_Checker {
 	 * @return bool|WP_Error
 	 */
 	public function check() {
-		if ( version_compare( $this->current_php_version, $this->support_php_version ) >= 0 )  {
+		if ( version_compare( $this->current_php_version, $this->support_php_version ) >= 0 ) {
 			return true;
 		}
 
